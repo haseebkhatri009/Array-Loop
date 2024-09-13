d8247c82bfd382b5b18246237d2b9612b1b93b1b
Haseeb Khatri <153817119+haseebkhatri009@users.noreply.github.com>
2024-09-13 12:32:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const myArr = [
+    [1],
+    [2,3],
+    [4,5,6],
+    [7,8]
+]
+for (let i = 0; i < myArr.length; i++) {
+    for (let j = 0; j < myArr[i].length; j++) {
+        console.log(myArr[i][j]);
+    }
+}
